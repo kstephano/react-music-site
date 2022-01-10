40ddb1f782fc960781d955380a54d9d9225df90c
Kelvin Stephano <kstephano@gmail.com>
2022-01-10 17:28:38 +0000

    fixed delete and added uuid for create

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,14 +1,16 @@
 import { combineReducers } from 'redux';
+const { uuid } = require('uuidv4');
 
+import { artistData } from '../data/artists';
 import { CREATE_ARTIST, DELETE_ARTIST, UPDATE_ARTIST } from './actions';
 import { merge } from '../helpers/index';
 
-const artistReducer = (state = [], action) => {
+const artistReducer = (state = artistData, action) => {
     switch (action.type) {
         case CREATE_ARTIST:
-            return [...state, { ...actionpayload }];
+            return [...state, { id: uuid(), ...action.payload}];
         case DELETE_ARTIST:
-            return state.filter(artist => artist.id != action.payload.id);
+            return state.filter(artist => artist.id != action.payload);
         case UPDATE_ARTIST:
             return state.map((artist) => {
                 return artist.id === action.payload.id ? merge(artist, action.payload) : artist
