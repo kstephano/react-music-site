56d690dd8a8a13c600e8ce8daa110339f8d43911
Kelvin Stephano <kstephano@gmail.com>
2022-01-10 12:08:56 +0000

    changed layout and added footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 
+import './App.css';
+
 const App = () => {
     const navigate = useNavigate();
     return (
-        <>
-            <h1>Awesome React Music Website</h1>
+        <div className="container">
             <nav className="nav-bar">
-                <Link to="/">Home</Link>
-                <Link to="/lyrics">Lyrics Generator</Link>
-                <Link to="/artist">Artist</Link>
-                <button onClick={() => navigate(-1)}>Go back</button>
+                <Link className="text-link" to="/">Home</Link>
+                <Link className="text-link" to="/lyrics">Lyrics Generator</Link>
+                <Link className="text-link" to="/artist">Artist</Link>
+                <button className="go-back" onClick={() => navigate(-1)}>Go Back</button>
             </nav>
-        </>
+            <div className="home-page">
+                <h1 aria-label="title">Musicly</h1>
+                <p>Welcome to Musicly, a website built for browsing different music. This app is currently in production and has limited features. 
+                    Visit the Lyrics Generator page to retrieve lyrics for a song. Or, view the Artist page to see information and discography for an
+                    artist; currently only displaying information for Logic. 
+                </p>
+            </div>
+            <div className="footer">
+                <p>Created by @kstephano. Built with React.</p>
+            </div>
+        </div>
     )
 }
 
